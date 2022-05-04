4da937a1b642b6e21fa5a6cd871ef121ec70b075
Richard Ogujawa <richardogujawa7@gmail.com>
2022-05-04 03:30:11 +0100

    created a design system

---
+++ markdown/checklist.md
@@ -0,0 +1,13 @@
+# Checklist
+- ~Gather all the assets~
+- ~Separate out the design into its components on Adobe XD~
+- ~Create a CSS design system (variables, utility classes, mini components)~
+- Check CSS regularly, even before designing the website check the design system and make sure it's all valid and up-to-date CSS, using the <a href="https://jigsaw.w3.org/css-validator/validator">W3C online validator</a>
+- Check HTML with this <a href="https://validator.w3.org/"> W3C online validator</a>
+- Build the website out using Firefox and it's dev tools
+- Ensure it's responsive (phones, tablets, laptops)
+- Ensure cross-platform compatability
+- Ensure accessibility by checking WCAG 2.0
+- Ensure all links work
+- Performance (minify code, use transforms instead of animating properties directly, using images that are less thna 1MB)
+
--- script/featured-preload.js
+++ script/featured-preload.js
@@ -22,14 +22,14 @@ function loadPage(){
 
     //*H1
     
-    //**H1Two and H1Three
-    setTimeout(h1TwoThreeAnim, 900);
+    // //**H1Two and H1Three
+    // setTimeout(h1TwoThreeAnim, 900);
 
-    //**H1One
-    setTimeout(h1OneAnim, 925);
+    // //**H1One
+    // setTimeout(h1OneAnim, 925);
 
-    //*H1Four 
-    setTimeout(h1FourAnim, 950);
+    // //*H1Four 
+    // setTimeout(h1FourAnim, 950);
     
 
     
@@ -83,20 +83,20 @@ function hamburgerIconAnim(){
 
 /*FEATURED PAGE ONLY*/
 //H1One
-function h1OneAnim(){
-    h1One.style.transform = "translateX(0%) rotateY(0deg)";
-    h1One.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
-}
+// function h1OneAnim(){
+//     h1One.style.transform = "translateX(0%) rotateY(0deg)";
+//     h1One.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
+// }
 
-function h1TwoThreeAnim(){
-    h1Two.style.transform = "translateY(0%)";
-    h1Two.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
+// function h1TwoThreeAnim(){
+//     h1Two.style.transform = "translateY(0%)";
+//     h1Two.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
     
-    h1Three.style.transform = "translateY(0%)";
-    h1Three.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
-}
+//     h1Three.style.transform = "translateY(0%)";
+//     h1Three.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
+// }
 
-function h1FourAnim(){
-    h1Four.style.transform = "translateX(0%) rotateY(0deg)";
-    h1Four.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
-}
+// function h1FourAnim(){
+//     h1Four.style.transform = "translateX(0%) rotateY(0deg)";
+//     h1Four.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
+// }
